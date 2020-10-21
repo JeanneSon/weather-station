4d0ea4e76c622a6db3d07dd12aa4506a0f715a06
JeanneSon <43119728+JeanneSon@users.noreply.github.com>
2020-10-21 20:17:31 +0200

    add Sensor info function and attributes

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,3 +1,23 @@
 public class Sensor {
-    
+    private final int productId;
+    private final int vendorId;
+    private final String location;
+
+    public Sensor(int productId, int vendorId, String location) {
+        this.productId = productId;
+        this.vendorId = vendorId;
+        this.location = location;
+    }
+
+    public String info() {
+        return "ProductId: " + this.productId + ", VendorId: " + this.vendorId + ", Standort " + this.location;
+    }
+
+    /**
+     * start application
+     * init Sensor
+     * init Station
+     * link Sensor with Station
+     * 
+     */
 }
