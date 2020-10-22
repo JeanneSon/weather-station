3bbbe71e3b0ce20487ffce40095b096d9468bcfe
JeanneSon <43119728+JeanneSon@users.noreply.github.com>
2020-10-22 11:05:43 +0200

    add random value generator

--- src/Sensor.java
+++ src/Sensor.java
@@ -1,8 +1,14 @@
-public class Sensor {
+
+
+// class is final as no class should inherit from this class
+public final class Sensor {
+    // attributes are final as they can only be set once when the Sensor is instanciated
     private final int productId;
     private final int vendorId;
     private final String location;
-
+    private static final double MAX_TEMP =  50.0; 
+    private static final double MIN_TEMP = -20.0; 
+    
     public Sensor(int productId, int vendorId, String location) {
         this.productId = productId;
         this.vendorId = vendorId;
@@ -13,6 +19,9 @@ public class Sensor {
         return "ProductId: " + this.productId + ", VendorId: " + this.vendorId + ", Standort " + this.location;
     }
 
+    private double generateValue() {
+        return MIN_TEMP + (MAX_TEMP - MIN_TEMP) * ImportManager.generateRandomDouble();
+    }
     /**
      * start application
      * init Sensor
