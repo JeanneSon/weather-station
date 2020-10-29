5bc3474e99c883225a2d591baaec1d7f92d899bc
JulianK <juliankrug123@gmail.com>
2020-10-29 23:43:05 +0100

    Weatherstation Tests added

--- test/WSTest.java
+++ test/WSTest.java
@@ -2,6 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,11 +28,14 @@ class WSTest {
 	void tearDown() throws Exception {
 		ws = null;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------
 
 	private WeatherStation ws;
 
 	@Test
-	void testGetCurrentTemp0() {
+	void testGetCurrentTemp0() throws Exception {
+
 		ws.setCurrentTemp(0.0, 3000);
 		assertEquals(0.0, ws.getCurrentTemp());
 
@@ -39,38 +43,200 @@ class WSTest {
 
 	@ParameterizedTest
 	@ValueSource(doubles = { 1.0, 2.0, 3.0, 4.0 })
-	void testGetCurrentTemp(double arg) {
+	void testGetCurrentTemp1(double arg) throws Exception {
 
 		ws.setCurrentTemp(arg, 3000);
 		assertEquals(arg, ws.getCurrentTemp());
 	}
 
-//verify : reset -> exception for temp request on->set->reset->getTemp
-//funktional requirement : wetterstation kann alle temperaturen darstellen
-
-	//für jede Folge eigene Testfkt über Array
-	//oder Testdaten in Array von arrays auslagern [erwartet im INdex 0]
 	@ParameterizedTest
-	@ValueSource(doubles = { 1.0, 2.0, 3.0 })
-	void testGetMinTemp(double arg) {
+	@ValueSource(doubles = { -Double.MAX_VALUE, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, Double.MAX_VALUE })
+	void testGetCurrentTemp2(double arg) throws Exception {
 
-		ws.setCurrentTemp(arg, 3000);
-		assertEquals(arg, ws.getMinTemp());
-	}
+		double[] dummyValues = { 0.0, 1.0, 2.0, 3.0, 4.0, -70000.8888, 76578765.465747 };
+		for (double d : dummyValues) {
+			ws.setCurrentTemp(d, 0);
+		}
 
-	@ParameterizedTest
-	@ValueSource(doubles = { 1.0, 2.0, 3.0 })
-	void testGetMaxTemp(double arg) {
+		ws.setCurrentTemp(arg, 0);
+		assertEquals(arg, ws.getCurrentTemp());
 
-		ws.setCurrentTemp(arg, 3000);
-		assertEquals(arg, ws.getMaxTemp());
 	}
 
 	@Test
-	void testMinMaxInfo() {
+	void testGetCurrentTemp_notMeassuredException() {
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getCurrentTemp();
+		});
 
-		ws.setCurrentTemp(1.0, 3000);
-		ws.setCurrentTemp(2.0, 3000);
-		assertEquals("Minimum: 1.0  Maximum: 2.0", ws.minMaxInfo());
 	}
+
+	// ----------------------------------------------------------------------------------------------------
+
+	@ParameterizedTest
+	@ValueSource(longs = { Long.MIN_VALUE, -10000, -1, 0, 1, 2, 3, 4, Long.MAX_VALUE })
+	void testGetCurrentTempTime(long arg) {
+
+		long[] dummyValues = { 0, 1, 2, 3, 4, -70000, 76578765 };
+		for (long l : dummyValues) {
+			ws.setCurrentTemp(0.0, l);
+		}
+
+		ws.setCurrentTemp(0.0, arg);
+		assertEquals(arg, ws.getCurrentTempTime());
+
+	}
+
+	@Test
+	void testGetCurrentTempTime_notMeassuredException() {
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getCurrentTempTime();
+		});
+
+	}
+
+	// ----------------------------------------------------------------------------------------------------
+	@ParameterizedTest
+	@ValueSource(doubles = { 0.0, -1.0, -2.0, -3.0, -4.0, -Double.MAX_VALUE })
+	void testGetMinTemp0(double arg) throws Exception {
+
+		double[] dummyValues = { 0.0, 1.0, 2.0, 3.0, 4.0, Double.MAX_VALUE };
+		for (double d : dummyValues) {
+			ws.setCurrentTemp(d, 0);
+		}
+		ws.setCurrentTemp(arg, 0);
+		assertEquals(arg, ws.getMinTemp());
+
+	}
+
+	@ParameterizedTest
+	@ValueSource(doubles = { 0.0, 1.0, 2.0, 3.0, 4.0, Double.MAX_VALUE })
+	void testGetMinTemp1(double arg) throws Exception {
+
+		double[] dummyValues = { 0.0, 1.0, 2.0, 3.0, 4.0, Double.MAX_VALUE };
+		for (double d : dummyValues) {
+			ws.setCurrentTemp(d, 0);
+		}
+		ws.setCurrentTemp(arg, 0);
+		assertEquals(0.0, ws.getMinTemp());
+
+	}
+
+	@ParameterizedTest
+	@ValueSource(doubles = { 0.0, -1.0, -2.0, -3.0, -4.0, -Double.MAX_VALUE, Double.MAX_VALUE })
+	void testGetMinTemp2(double arg) throws Exception {
+
+		ws.setCurrentTemp(-Double.MAX_VALUE, 0);
+
+		ws.setCurrentTemp(arg, 0);
+		assertEquals(-Double.MAX_VALUE, ws.getMinTemp());
+
+	}
+
+	@Test
+	void testGetMinTemp_notMeassuredException() {
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getMinTemp();
+		});
+
+	}
+
+	// ----------------------------------------------------------------------------------------------------
+	@ParameterizedTest
+	@ValueSource(doubles = { 0.0, 1.0, 2.0, 3.0, 4.0, Double.MAX_VALUE })
+	void testGetMaxTemp0(double arg) throws Exception {
+
+		double[] dummyValues = { -0.0, -1.0, -2.0, -3.0, -4.0, -Double.MAX_VALUE };
+		for (double d : dummyValues) {
+			ws.setCurrentTemp(d, 0);
+		}
+		ws.setCurrentTemp(arg, 0);
+		assertEquals(arg, ws.getMaxTemp());
+
+	}
+
+	@ParameterizedTest
+	@ValueSource(doubles = { 0.0, -1.0, -2.0, -3.0, -4.0, -Double.MAX_VALUE })
+	void testGetMaxTemp1(double arg) throws Exception {
+
+		double[] dummyValues = { 0.0, -1.0, -2.0, -3.0, -4.0, -Double.MAX_VALUE };
+		for (double d : dummyValues) {
+			ws.setCurrentTemp(d, 0);
+		}
+		ws.setCurrentTemp(arg, 0);
+		assertEquals(0.0, ws.getMaxTemp());
+
+	}
+
+	@ParameterizedTest
+	@ValueSource(doubles = { 0.0, 1.0, 2.0, 3.0, 4.0, Double.MAX_VALUE, -Double.MAX_VALUE })
+	void testGetMaxTemp2(double arg) throws Exception {
+
+		ws.setCurrentTemp(Double.MAX_VALUE, 0);
+
+		ws.setCurrentTemp(arg, 0);
+		assertEquals(Double.MAX_VALUE, ws.getMaxTemp());
+
+	}
+
+	@Test
+	void testGetMaxTemp_notMeassuredException() {
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getMaxTemp();
+		});
+
+	}
+	// ----------------------------------------------------------------------------------------------------
+
+	@ParameterizedTest
+	@ValueSource(doubles = { 0.0, 1.0, 2.0, 3.0, 4.0, Double.MAX_VALUE })
+	void testreset0(double arg) throws Exception {
+
+		ws.setCurrentTemp(arg, 0);
+
+		ws.reset();
+
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getCurrentTemp();
+		});
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getCurrentTempTime();
+		});
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getMinTemp();
+		});
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getMaxTemp();
+		});
+
+		assertEquals(false, ws.getMeasuredOneValid());
+
+	}
+
+	@Test
+	void testreset1() throws Exception {
+		double[] dummyValues = { 0.0, -1.0, 2.0, -3.0, 4.0, -Double.MAX_VALUE, Double.MAX_VALUE };
+		for (double d : dummyValues) {
+			ws.setCurrentTemp(d, 0);
+		}
+
+		ws.reset();
+
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getCurrentTemp();
+		});
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getCurrentTempTime();
+		});
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getMinTemp();
+		});
+		Assertions.assertThrows(Exception.class, () -> {
+			ws.getMaxTemp();
+		});
+
+		assertEquals(false, ws.getMeasuredOneValid());
+
+	}
+
 }
