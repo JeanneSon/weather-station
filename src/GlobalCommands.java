c8c7f7d9f1db83e3d8510c73145caebd0ac1b8d1
JulianK <juliankrug123@gmail.com>
2020-11-04 17:36:56 +0100

    DATA 3 is now Minimum

--- src/GlobalCommands.java
+++ src/GlobalCommands.java
@@ -11,7 +11,7 @@ public abstract class GlobalCommands {
 
     public static final String INFO_COMMAND = "INFO";
     public static final String DATA_COMMAND = "DATA";
-    public static final String DATA_COMMAND_REGEX = "DATA [1-9][0-9]*";
+    public static final String DATA_COMMAND_REGEX = "DATA [3-9][0-9]*";
     public static final String DATA_STOP_COMMAND = "DATA STOP";
     public static final String STOP_SENSOR_REQUEST = "STOP REQUEST";
 
